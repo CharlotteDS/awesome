6937d6d83064a140cc9932f77d281217c6b523b0
Kazuki-Kana <53716028+Kazuki-Kana@users.noreply.github.com>
2019-08-12 12:11:30 +0800

    [Function] Log-out (#6)
    * [Function] Log-out
    
    * fix indention

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,5 +6,13 @@ module SessionsHelper
   def logged_in?
     !current_user.nil?
   end
+
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
+
+  def log_out
+    session.delete(:user_id)
+  end
     
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get '/signup', to: 'users#new'
   get '/login', to: 'sessions#new'
-
+  delete '/logout', to: 'sessions#destroy'
+  
   resources :users
   resources :sessions
 end
