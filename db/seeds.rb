5ad85b66788270ef405b30e282907691b4642070
Kazuki-Kana <53716028+Kazuki-Kana@users.noreply.github.com>
2019-08-14 09:27:42 +0800

    [Layout] Create User Lisrt (#7)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(
+  name: "Kazuki",
+  email: "fukuroushikuang@gmail.com",
+  password: "Abcd1234"
+)
+
+50.times do |n|
+  User.create!(
+    name: Faker::JapaneseMedia::OnePiece.character,
+    email: Faker::Internet.unique.email,
+    password: "password"
+  )
+end
