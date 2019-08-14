420e74834fe95955b1383bae34c8eedfc1eec82f
Kazuki-Kana <53716028+Kazuki-Kana@users.noreply.github.com>
2019-08-14 10:43:25 +0800

    [Layout] Create Profile (#8)
    * [Layout] Create Profile
    
    * Fix indention
    
    * Fix indention for show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,10 @@ class UsersController < ApplicationController
     @users = User.paginate(page: params[:page], per_page: 10)
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
     @user = User.new
   end
